fe27f1568197c166e21e28047f8cd12c2b3d5139
Everton Fernandes <evertonfernandes.jsx@gmail.com>
2024-06-25 21:19:39 -0300

    feat: :sparkles: add width and height on image

--- landmarks/CircleImage.swift
+++ landmarks/CircleImage.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtle")
+            .frame(width: 350, height: 350)
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
             }
             .shadow(radius: 7)
+          
     }
 }
 
